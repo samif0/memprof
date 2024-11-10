1735e8b90f5a2e57bf44ba09c4641f47a959f615
Sami Fawcett <samifawcett.nyc@gmail.com>
2024-11-09 21:57:27 -0800

    add clap, experiment with ps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+use std::process::Command;
+//use mach; TODO: Use syscalls instead of ps call
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    pid: i32,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    
+    let output = Command::new("ps")
+        .args(["-p", &args.pid.to_string(), "-o", "rss"])
+        .output()
+        .expect("Failed to execute ps");
+
+    if let Ok(output_str) = String::from_utf8(output.stdout) {
+        if let Some(memory) = output_str.lines().nth(1) {
+            println!("Memory usage: {} KB", memory.trim());
+        }
+    }
 }
